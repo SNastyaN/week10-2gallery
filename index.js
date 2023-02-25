671d29c731996a123b25e7db9b985fa557ba4634
Anastasiia Sedalishcheva <ffttyyll@gmail.com>
2023-02-25 16:42:11 +0900

    галерея

---
+++ index.js
@@ -0,0 +1,9 @@
+function changeImg(){
+    let image = document.getElementById('sea');
+image.src="1.jpg";
+}
+
+function changeImg2(){
+    let image = document.getElementById('sea');
+image.src="sea.jpeg";
+}
